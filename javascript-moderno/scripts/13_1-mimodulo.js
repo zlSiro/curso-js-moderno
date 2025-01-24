98ac87d2fea72c85d6ab82eef5b019d722e674e9
Juan Pablo <jpablo.valdebenitom@gmail.com>
2025-01-24 00:24:20 -0300

    module of 13.0

---
+++ javascript-moderno/scripts/13_1-mimodulo.js
@@ -0,0 +1,14 @@
+export function imprimirArray(datos) {
+    datos.forEach(element => {
+        console.log(element);
+    });
+
+    return datos
+}
+
+ export function saludo(nombre) {
+    console.log("Hola, ",nombre);
+
+    return nombre;
+}
+
