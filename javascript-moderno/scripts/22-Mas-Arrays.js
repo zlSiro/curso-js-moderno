ae62c0bce7ac7d2eec6d7e27fdfc1b058c6219aa
Juan Pablo <jpablo.valdebenitom@gmail.com>
2025-01-24 19:07:21 -0300

    Mas metodos de arrays

---
+++ javascript-moderno/scripts/22-Mas-Arrays.js
@@ -0,0 +1,38 @@
+let personas = ["Juan Pablo", "Claudia", "Jeremy", "Diana", "Gia"];
+
+let paises = [{
+    nombre: "Chile",
+    continente: "America"
+},
+{
+    nombre: "Ecuador",
+    continente: "America"
+},
+{
+    nombre: "Brasil",
+    continente: "America"
+}
+];
+
+// Metodo SOME
+let existe = personas.some(nombre => nombre === "Juan Pablo"); // devuelve true o false dependiendo si existe o no
+console.log(existe)
+
+
+// Metodo EVERY
+let esIgualEnTodos = paises.every(pais => pais.continente === "America"); // devuelve true o false dependiendo si existe o no
+
+
+// Metodo MAP
+let nuevas_personas = personas.map(persona => {
+    let nueva = persona + " Hola";
+    console.log(nueva);
+    return nueva;
+}); // Es lo mismo que hacer un ForEACH. la diferencia es que el map devuelfve el array y el forEach no devuelve nada.
+
+
+// Metodo REDUCE
+console.log(personas.reduce((acumulador, actual) => {
+    let valorActual = ", " + actual
+    return acumulador + valorActual
+}));
