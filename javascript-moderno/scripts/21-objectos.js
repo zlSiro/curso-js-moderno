1a0ff3217cbb7924847e1337c954f18582b52d29
Juan Pablo <jpablo.valdebenitom@gmail.com>
2025-01-24 19:07:12 -0300

    Nueva forma de manipular objetos

---
+++ javascript-moderno/scripts/21-objectos.js
@@ -0,0 +1,20 @@
+let titulo = "StarWars";
+let genero = "Ciencia Ficcion";
+let duracion = "2 Horas"
+
+const pelicula = {
+    titulo,
+    genero,
+    duracion,
+    [genero + "2022"]: "Hola",
+    mostrar() {
+        return this.titulo + ". Genero: " + this.genero
+    }
+};
+
+console.log(pelicula);
+console.log(pelicula.mostrar());
+console.log(Object.values(pelicula));
+
+// Este metodo convierte el Objeto en un objeto iterable
+Object.entries(pelicula).forEach(item => console.log(item));
