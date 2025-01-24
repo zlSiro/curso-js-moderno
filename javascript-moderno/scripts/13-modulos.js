3b78aefd0035ce61bfaa66a30fee104a592dd5b5
Juan Pablo <jpablo.valdebenitom@gmail.com>
2025-01-24 00:24:09 -0300

    modules

---
+++ javascript-moderno/scripts/13-modulos.js
@@ -0,0 +1,9 @@
+console.log("Archivo Main de mi proyecto")
+
+// aca importamos el modulo con sus funciones
+
+import { imprimirArray, saludo } from './13_1-mimodulo.js';
+
+let nombres = ["Juan Pablo", "Claudia", "Jeremy"];
+imprimirArray(nombres)
+saludo("Juan Pablo")
