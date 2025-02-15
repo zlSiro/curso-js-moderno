5d0bedf71af196d14b8b838a22436ba02a0f03fb
Juan Pablo <jpablo.valdebenitom@gmail.com>
2025-02-15 17:55:28 -0300

    otros

---
+++ javascript-moderno/scripts/25-otros.js
@@ -0,0 +1,23 @@
+// SEPARADORES NUMERICOS
+
+let numero = 4_000_000; // No afectan en nada el numero
+console.log(numero)
+
+// REPLACE Y REPLACE ALL
+
+let frase = "Voy en mi auto feliz con la Claudia y veo otro auto";
+let nuevaFrase = frase.replace("auto", "moto");
+let fraseFinal = frase.replaceAll("auto", "Moto"); // cambia todas las coincidencias en el string
+
+console.log(frase);
+console.log(nuevaFrase);
+console.log(fraseFinal);
+
+// ASIGNADORES LOGICOS
+
+let nombre = "";
+let id = 37;
+
+nombre ||= id // si existe nombre te devuelve el nombre, si no, el ID
+console.log(nombre)
+
